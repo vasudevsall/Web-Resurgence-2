a893015e3c46476fd69d02d0bba23976114c3e6f
Vasu Dev Sall <vasudevsall@gmail.com>
2020-04-02 16:26:47 +0530

    Added loader and animation

--- js/index.js
+++ js/index.js
@@ -1,6 +1,31 @@
+//navBar javascript code
+
+$(window).scroll(function() {
+    if ($(document).scrollTop() > 50) {
+        $('.nav').addClass('affix');
+        console.log("OK");
+    } else {
+        $('.nav').removeClass('affix');
+    }
+});
 $('.navTrigger').click(function () {
     $(this).toggleClass('active');
     console.log("Clicked menu");
     $("#mainListDiv").toggleClass("show_list");
     $("#mainListDiv").fadeIn();
 });
+
+//The loading of page
+$('#main-content').hide();
+document.addEventListener("DOMContentLoaded", function(event){
+  document.getElementById("loadingVideo").addEventListener('ended',function() {
+    // console.log("Hi");
+    $(window).on('load', afterLoad());
+  });
+});
+
+function afterLoad() {
+  $('#loader').fadeOut(1000, function(){
+    $('#main-content').fadeIn(1000);
+  });
+}
