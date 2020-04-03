3add5db0e9261d95a6df9afe1b851ed413f26f4f
Vasu Dev Sall <vasudevsall@gmail.com>
2020-04-03 16:54:43 +0530

    Page Change Transitions done

--- js/index.js
+++ js/index.js
@@ -1,3 +1,18 @@
+var pages = ['/content/home.html #home-changing-content',
+             '/content/events.html #events-changing-content',
+             '/content/team.html #team-changing-content',
+             '/content/about.html #about-changing-content',
+             '/content/contact.html #contact-changing-content',
+             '/content/sponsor.html #sponsor-changing-content'];
+
+var currentPage = 1;
+var paheHit = 1;
+var allowTransition = 1;
+/*This variable is to prevent user from clicking on another
+page before the current page is loaded.
+0 means transition is not allowed
+and 1 means transition is allowed.*/
+
 //navBar javascript code
 
 $(window).scroll(function() {
@@ -12,8 +27,14 @@ $('.navTrigger').click(function () {
     $(this).toggleClass('active');
     console.log("Clicked menu");
     $("#mainListDiv").toggleClass("show_list");
-    $("#mainListDiv").fadeIn();
+    $("#mainListDiv").fadeIn("slow");
 });
+
+function removeNav() {
+    $(".navTrigger").toggleClass('active');
+    $("#mainListDiv").toggleClass("show_list");
+    // $("#mainListDiv").fadeToggle("slow");
+}
 
 //The loading of page
 document.addEventListener("DOMContentLoaded", function(event){
@@ -24,9 +45,171 @@ document.addEventListener("DOMContentLoaded", function(event){
 });
 
 function afterLoad() {
-  $("#changing-content").load("/content/home.html #home-changing-content");
+  $("#changing-content").load(pages[currentPage-1]);
   $('#main-content').removeClass('invisible').hide();
   $('#loader').fadeOut(1000, function(){
     $('#main-content').fadeIn(1000);
+  });
+}
+
+function home() {
+  if(allowTransition == 1 && currentPage!=1) {
+    removeNav();
+    pageHit = 1;
+    allowTransition = 0;
+    transitionFunction();
+  }
+}
+
+function events() {
+  if(allowTransition == 1 && currentPage!=2) {
+    removeNav();
+    pageHit = 2;
+    allowTransition = 0;
+    transitionFunction();
+  }
+}
+
+function team() {
+  if(allowTransition == 1 && currentPage!=3) {
+    removeNav();
+    pageHit = 3;
+    allowTransition = 0;
+    transitionFunction();
+  }
+}
+
+function about() {
+  if(allowTransition == 1 && currentPage!=4) {
+    removeNav();
+    pageHit = 4;
+    allowTransition = 0;
+    transitionFunction();
+  }
+}
+
+function contact() {
+  if(allowTransition == 1 && currentPage!=5) {
+    removeNav();
+    pageHit = 5;
+    allowTransition = 0;
+    transitionFunction();
+  }
+}
+
+function sponsor() {
+  if(allowTransition == 1 && currentPage!=6) {
+    removeNav();
+    pageHit = 6;
+    allowTransition = 0;
+    transitionFunction();
+  }
+}
+
+function transitionFunction() {
+  console.log(pageHit);
+  if(pageHit < currentPage){
+    slideOutRight("#changing-content");
+    setTimeout(function(){
+      //Change Body Background based on the page
+      if (pageHit == 2) {
+        $("#theBody").removeClass("cardBody");
+        $("#theBody").removeClass("homeBody");
+        $("#theBody").addClass("eventBody");
+      }
+      else if(pageHit == 4) {
+        console.log("Hi");
+        $("#theBody").removeClass("eventBody");
+        $("#theBody").removeClass("homeBody");
+        $("#theBody").addClass("cardBody");
+      }
+      else {
+        $("#theBody").removeClass("eventBody");
+        $("#theBody").removeClass("cardBody");
+        $("#theBody").addClass("homeBody");
+      }
+      //Sliding in functions
+      $("#changing-content").css("left","-100vw");
+      $("#changing-content").load(pages[pageHit-1]);
+      slideInLeft("#changing-content");
+      transitionEnd();
+    }, 550);
+  }
+  else if (pageHit > currentPage) {
+    slideOutLeft("#changing-content");
+    setTimeout(function(){
+      //Change Body Background based on the page
+      if (pageHit == 2) {
+        $("#theBody").removeClass("cardBody");
+        $("#theBody").removeClass("homeBody");
+        $("#theBody").addClass("eventBody");
+      }
+      else if(pageHit == 4) {
+        console.log("Hi");
+        $("#theBody").removeClass("eventBody");
+        $("#theBody").removeClass("homeBody");
+        $("#theBody").addClass("cardBody");
+        console.log(document.getElementById("theBody"));
+      }
+      else {
+        $("#theBody").removeClass("eventBody");
+        $("#theBody").removeClass("cardBody");
+        $("#theBody").addClass("homeBody");
+      }
+      //Sliding in functions
+      $("#changing-content").css("left","100vw");
+      $("#changing-content").show();
+      $("#changing-content").load(pages[pageHit-1]);
+      slideInRight("#changing-content");
+      transitionEnd();
+    }, 550);
+  }
+  else {
+    transitionEnd();
+  }
+}
+function transitionEnd() {
+  currentPage = pageHit;
+  allowTransition = 1;
+}
+
+function slideInRight(theId) {
+	console.log("1");
+  // $(theId).show();
+  $(theId).css("left","100vw");
+  $(theId).css("opacity","0");
+  $(theId).animate({
+    left: '0vw',
+    opacity: 1
+  }, 500);
+}
+function slideInLeft(theId) {
+	console.log("2");
+  // $(theId).show();
+  $(theId).css("left","-100vw");
+  $(theId).css("opacity","0");
+  $(theId).animate({
+    left: '0vw',
+    opacity: 1
+  }, 500);
+}
+
+function slideOutRight(theId) {
+	console.log("3");
+  $(theId).animate({
+    left: '100vw',
+    opacity: 0
+  }, 500, function(){
+    // $(theId).hide();
+  });
+}
+
+function slideOutLeft(theId) {
+	console.log("4");
+  $(theId).animate({
+    left: '-100vw',
+    opacity: 0
+  }, 500, function(){
+    // $(theId).hide();
   });
 }
