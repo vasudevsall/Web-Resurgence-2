4fdc793b27e859917aa86f0bfd55fd586c89e2f8
Vasu Dev Sall <vasudevsall@gmail.com>
2020-04-07 16:50:08 +0530

    Changes

--- js/events.js
+++ js/events.js
@@ -1,4 +1,4 @@
-// var eventsChange = $("#changing-events");
+var checkVariable = 0;
 
 //Intra-University Page Functions
 
@@ -15,18 +15,132 @@ function unHover(element, image) {
 function intraUnivClick(){
   // console.log("HiHIHIhI");
   $("#changing-events").fadeOut(500,function(){
+    console.log("HiHiHiHiHI");
     $("#changing-events").css("paddingTop", "0px");
     $("#changing-events").load("/content/events1.html #changing-events-intra", function(){
+      console.log("Hi2");
       $("#changing-events").fadeIn(500);
     });
   });
 }
 
 function backEvents() {
-  $("#changing-events").fadeOut(500,function(){
-    $("#changing-events").css("paddingTop", "0px");
-    $("#changing-events").load("/content/events.html #changing-events", function(){
-      $("#changing-events").fadeIn(500);
+  if(checkVariable == 0)
+  {
+    $("#changing-events").fadeOut(500,function(){
+      $("#changing-events").css("paddingTop", "0px");
+      $("#changing-events").load("/content/events.html #changing-events", function(){
+        $("#changing-events").fadeIn(500);
+      });
+    });
+  }
+}
+
+function dance() {
+  checkVariable = 1;
+  eventsTransition('/images/danceImage.gif');
+  console.log("Dance");
+}
+
+function theatre() {
+  checkVariable = 1;
+  eventsTransition('/images/theatreImage.gif');
+  console.log("Theatre");
+}
+
+function music() {
+  checkVariable = 1;
+  eventsTransition('/images/musicImage.gif');
+  console.log("Music");
+}
+
+function photo() {
+  checkVariable = 1;
+  eventsTransition('/images/photoImage.gif');
+  console.log("Photo");
+}
+
+function art() {
+  checkVariable = 1;
+  eventsTransition('/images/artImage.gif');
+  console.log("Art");
+}
+
+function literature() {
+  checkVariable = 1;
+  eventsTransition('/images/literatureImage.gif');
+  console.log("Literature");
+}
+
+function special() {
+  checkVariable = 1;
+  eventsTransition('/images/specialImage.gif');
+  console.log("Special");
+}
+
+function eventsTransition(imageId) {
+  console.log("Hi2Hi2");
+  $("#changing-events-intra").fadeOut(500, function(){
+    $("#changing-events-intra").load("/content/events2.html #changing-events-intra-2", function(){
+      $("#changing-events-intra").fadeIn(500);
+      checkVariable = 0;
     });
   });
 }
+
+//events2.html Js starts
+
+var subcontent1 = document.getElementById("subcontent1");
+var subcontent2 = document.getElementById("subcontent2");
+var subcontent3 = document.getElementById("subcontent3");
+var subcontent4 = document.getElementById("subcontent4");
+var subcontent5 = document.getElementById("subcontent5");
+var subcontent6 = document.getElementById("subcontent6");
+
+// var eventBubble1 = document.getElementById("subcontent1-p");
+// var eventBubble2 = document.getElementById("subcontent2-p");
+// var eventBubble3 = document.getElementById("subcontent3-p");
+// var eventBubble4 = document.getElementById("subcontent4-p");
+// var eventBubble5 = document.getElementById("subcontent5-p");
+// var eventBubble6 = document.getElementById("subcontent6-p");
+
+var textbox = document.getElementById("textbox");
+var blurDiv= document.getElementById('blurs');
+
+subcontent1.addEventListener("click", function(){
+    textbox.classList.remove("invisible");
+    blurDiv.classList.remove("invisible");
+});
+
+subcontent2.addEventListener("click", function(){
+    textbox.classList.remove("invisible");
+    blurDiv.classList.remove("invisible");
+});
+
+subcontent3.addEventListener("click", function(){
+    textbox.classList.remove("invisible");
+    blurDiv.classList.remove("invisible");
+});
+
+subcontent4.addEventListener("click", function(){
+    textbox.classList.remove("invisible");
+    blurDiv.classList.remove("invisible");
+});
+
+subcontent5.addEventListener("click", function(){
+    textbox.classList.remove("invisible");
+    blurDiv.classList.remove("invisible");
+});
+
+subcontent6.addEventListener("click", function(){
+    textbox.classList.remove("invisible");
+    blurDiv.classList.remove("invisible");
+});
+
+blurDiv.addEventListener("click", function(){
+    textbox.classList.add("invisible");
+    blurDiv.classList.add("invisible");
+
+});
+
+//events2.html Js ends
