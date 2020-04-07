faff73956a6fbaa4a27c434331052cce26b7bea8
Vasu Dev Sall <vasudevsall@gmail.com>
2020-04-07 17:07:07 +0530

    Changes made

--- js/events.js
+++ js/events.js
@@ -79,9 +79,9 @@ function special() {
 }
 
 function eventsTransition(imageId) {
-  console.log("Hi2Hi2");
   $("#changing-events-intra").fadeOut(500, function(){
     $("#changing-events-intra").load("/content/events2.html #changing-events-intra-2", function(){
+      document.getElementById("eventImg").src = imageId;
       $("#changing-events-intra").fadeIn(500);
       checkVariable = 0;
     });
@@ -90,12 +90,12 @@ function eventsTransition(imageId) {
 
 //events2.html Js starts
 
-var subcontent1 = document.getElementById("subcontent1");
-var subcontent2 = document.getElementById("subcontent2");
-var subcontent3 = document.getElementById("subcontent3");
-var subcontent4 = document.getElementById("subcontent4");
-var subcontent5 = document.getElementById("subcontent5");
-var subcontent6 = document.getElementById("subcontent6");
+// var subcontent1 = document.getElementById("subcontent1");
+// var subcontent2 = document.getElementById("subcontent2");
+// var subcontent3 = document.getElementById("subcontent3");
+// var subcontent4 = document.getElementById("subcontent4");
+// var subcontent5 = document.getElementById("subcontent5");
+// var subcontent6 = document.getElementById("subcontent6");
 
 // var eventBubble1 = document.getElementById("subcontent1-p");
 // var eventBubble2 = document.getElementById("subcontent2-p");
@@ -107,35 +107,11 @@ var subcontent6 = document.getElementById("subcontent6");
 var textbox = document.getElementById("textbox");
 var blurDiv= document.getElementById('blurs');
 
-subcontent1.addEventListener("click", function(){
-    textbox.classList.remove("invisible");
-    blurDiv.classList.remove("invisible");
-});
-
-subcontent2.addEventListener("click", function(){
-    textbox.classList.remove("invisible");
-    blurDiv.classList.remove("invisible");
-});
-
-subcontent3.addEventListener("click", function(){
-    textbox.classList.remove("invisible");
-    blurDiv.classList.remove("invisible");
-});
-
-subcontent4.addEventListener("click", function(){
-    textbox.classList.remove("invisible");
-    blurDiv.classList.remove("invisible");
-});
-
-subcontent5.addEventListener("click", function(){
-    textbox.classList.remove("invisible");
-    blurDiv.classList.remove("invisible");
-});
-
-subcontent6.addEventListener("click", function(){
-    textbox.classList.remove("invisible");
-    blurDiv.classList.remove("invisible");
-});
+function bubbleClick(bubbleNumber) {
+  var theBubble = "subcontent"+bubbleNumber;
+  blurDiv.classList.remove("invisible");
+  textbox.classList.remove("invisible");
+}
 
 blurDiv.addEventListener("click", function(){
     textbox.classList.add("invisible");
